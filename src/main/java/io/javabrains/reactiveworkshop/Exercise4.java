993eabfd0c4a429ac1a1a93948cd7588c9595111
Reggie <reggie.senal@gmail.com>
2024-08-19 11:11:04 +0800

    answers to exercise 4

--- src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
@@ -8,11 +8,24 @@ public class Exercise4 {
 
         // Use ReactiveSources.intNumberMono()
 
-        // Print the value from intNumberMono when it emits
-        // TODO: Write code here
+        // Print the value from intNumberMono when it emists
+
+        ReactiveSources.intNumberMono().subscribe(value -> {
+            System.out.println("value: " + value);
+        }, error -> {
+            System.err.println("some error");
+            error.printStackTrace();
+        }, () -> {
+            System.out.println("completed subscribe");
+        });
+
 
         // Get the value from the Mono into an integer variable
-        // TODO: Write code here
+        // this blocks the call
+        Integer n = ReactiveSources.intNumberMono().block();
+        int value = n.intValue();
+        System.out.println("Integer: " + n);
+        System.out.println("Integer value: " + value);
 
         System.out.println("Press a key to end");
         System.in.read();
