85bd67c629941c26f94f1c77d696c464d0336107
Reggie <reggie.senal@gmail.com>
2024-08-20 14:49:44 +0800

    answer exercise 7

--- src/main/java/io/javabrains/reactiveworkshop/Exercise7.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise7.java
@@ -10,16 +10,43 @@ public class Exercise7 {
         // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
 
         // Print all values from intNumbersFlux that's greater than 5
-        // TODO: Write code here
+        // ReactiveSources.intNumbersFlux().log().toStream().filter(value -> value > 5).forEach(System.out::println);
+       /* ReactiveSources.intNumbersFlux()
+                .filter(value -> value > 5)
+                .log()
+                .subscribe(System.out::println);*/
 
         // Print 10 times each value from intNumbersFlux that's greater than 5
-        // TODO: Write code here
+       /* ReactiveSources.intNumbersFlux().log().toStream().filter(value -> value > 5).forEach(value -> {
+            int i = 0;
+            while (i++ < 10) {
+                System.out.println(value);
+            }
+        });*/
+        /*ReactiveSources.intNumbersFlux()
+                .filter(value -> value > 5)
+                .map(value -> value * 10)
+                .subscribe(System.out::println);*/
 
         // Print 10 times each value from intNumbersFlux for the first 3 numbers emitted that's greater than 5
-        // TODO: Write code here
+        /*ReactiveSources.intNumbersFlux()
+                .filter(value -> value > 5)
+                .map(value -> value * 10)
+                .take(3)
+                .subscribe(System.out::println);*/
 
         // Print each value from intNumbersFlux that's greater than 20. Print -1 if no elements are found
-        // TODO: Write code here
+        /*ReactiveSources.intNumbersFlux()
+                .filter(value -> value > 20)
+                .flatMap(value -> Mono.just(value))
+                .switchIfEmpty(Mono.just(-1))
+                .subscribe(System.out::println);*/
+
+        ReactiveSources.intNumbersFlux()
+                .log()
+                .filter(value -> value > 20)
+                .defaultIfEmpty(-1)
+                .subscribe(System.out::println);
 
         // Switch ints from intNumbersFlux to the right user from userFlux
         // TODO: Write code here
